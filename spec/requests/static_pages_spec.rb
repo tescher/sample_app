fd7871f470fb30a525ae064e4156a27b91908a4b
Tim Escher <tim@eschers.com>
2012-10-24 10:43:54 -0500

    Refactor tests to remove implementation

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,55 +4,40 @@ describe "Static pages" do
 
   subject { page }
 
-  shared_examples_for "all static pages" do
-    it { should have_selector('h1',    text: heading) }
-    it { should have_selector('title', text: full_title(page_title)) }
-  end
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading) { 'Sample App'}
-    let(:page_title) { '' }
-
-    it_should_behave_like "all static pages"
-    it { should_not have_selector 'title', text: '| Home' }
+    it { should have_valid_header_and_title('Sample App', '') }
+    it { should_not have_home_title }
   end
 
   describe "Help page" do
     before { visit help_path }
-    let(:heading) { 'Help'}
-    let(:page_title) { 'Help' }
+    it { should have_valid_header_and_title('Help', 'Help') }
 
-    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    let(:heading) { 'About'}
-    let(:page_title) { 'About Us' }
-
-    it_should_behave_like "all static pages"
+    it { should have_valid_header_and_title('About', 'About Us') }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    let(:heading) { 'Contact'}
-    let(:page_title) { 'Contact' }
-
-    it_should_behave_like "all static pages"
-  end
+    it { should have_valid_header_and_title('Contact', 'Contact') }
+ end
 
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
+    page.should have_valid_header_and_title(nil, 'About Us')
     click_link "Help"
-    page.should have_selector 'title', text: full_title('Help')
+    page.should have_valid_header_and_title(nil, 'Help')
     click_link "Contact"
-    page.should have_selector 'title', text: full_title('Contact')
+    page.should have_valid_header_and_title(nil, 'Contact')
     click_link "Home"
     click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign up')
+    page.should have_valid_header_and_title(nil, 'Sign up')
     click_link "sample app"
     page.should # fill in
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,29 @@
 include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_valid_header_and_title do |heading, page_title|
+  match do |page|
+    if (heading) then
+      page.should have_selector('h1', text: heading)
+    end
+    page.should have_selector('title', text: full_title(page_title))
+  end
+end
+
+RSpec::Matchers.define :have_home_title do
+  match do |page|
+    page.should have_selector('title', text: '| Home')
+  end
+end
+
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
